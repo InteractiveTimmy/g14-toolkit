c22be683bcb8c7ffaedb69606281deb635c4819a
InteractiveTimmy <interactivetimmy@gmail.com>
2021-01-08 15:37:38 -0500

    feat(app): add build templates

---
+++ app.build.js
@@ -0,0 +1,11 @@
+module.exports = {
+  appId: 'com.electron.g14tk',
+  directories: {
+    output: './app',
+  },
+  files: [
+    './app.index.js',
+    './resources/**/*',
+  ],
+  productName: 'G14 Toolkit'
+};
